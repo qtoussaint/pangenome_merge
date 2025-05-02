1d6ed3f0e3d6fc8d37c16329b0e82e50b76cdfab
Jacqueline Toussaint <lilyjacqueline@ebi.ac.uk>
2025-05-02 15:25:11 +0100

    fixed syntax error

---
+++ pangenomerge/run_mmseqs.py
@@ -0,0 +1,22 @@
+# run mmseqs easy-search
+def run_mmseqs_easysearch(
+        query,
+        target,
+        outdir,
+        tmpdir):
+
+    cmd = "mmseqs easy-search "
+    
+    cmd += str(query) + " "
+    
+    cmd += str(target) + " "
+    
+    cmd += str(outdir) + " " 
+
+    cmd += str(tmpdir)
+    
+    cmd += ' --search-type 2 --format-mode 4 -a --format-output "query,target,fident,nident,alnlen,qlen,tlen,evalue"'
+
+    subprocess.run(cmd, shell=True, check=True)
+
+    return
