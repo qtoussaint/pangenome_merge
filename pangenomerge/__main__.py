7861b61bdf9276c17e05ea44add0902cead792ca
Jackie Toussaint <114661621+qtoussaint@users.noreply.github.com>
2025-05-19 19:36:45 +0100

    fixed bitwise operator

--- pangenomerge/__main__.py
+++ pangenomerge/__main__.py
@@ -346,7 +346,7 @@ def main():
                 if not merged_graph.has_edge(edge[0], edge[1]):
                     merged_graph.add_edge(edge[0], edge[1])
         
-        if options.mode == 'test' & graph_count == (n_graphs-1):
+        if options.mode == 'test' and graph_count == (n_graphs-1):
 
             ### gather seqIDs to enable calculation of clustering metrics
             
