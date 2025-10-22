3bff3b46fa9cee579f8e313072067162383e0cbe
Jacqueline Toussaint <lilyjacqueline@ebi.ac.uk>
2025-10-22 16:12:07 +0100

    fix parameter designation

--- pangenomerge/__main__.py
+++ pangenomerge/__main__.py
@@ -614,9 +614,9 @@ def main():
         # debug statement...
         logging.debug(f"Before collapse: {len(merged_graph.nodes())} nodes")
 
-        # context search parameters defined in options
-        family_threshold = family_threshold  # sequence identity threshold
-        context_threshold = context_threshold  # contextual similarity threshold 
+        # context search parameters defined in arguments
+        family_threshold = parameters.family_threshold  # sequence identity threshold
+        context_threshold = parameters.context_threshold  # contextual similarity threshold 
 
         # one centroid to one sequence
         centroid_to_seq = {}
