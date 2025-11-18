ebdccde79efd032b91473e2ed3a28f4c3501af20
Jacqueline Toussaint <lilyjacqueline@ebi.ac.uk>
2025-11-18 15:17:23 +0000

    updating mmseqs workflow for speed

--- pangenomerge/run_mmseqs.py
+++ pangenomerge/run_mmseqs.py
@@ -1,26 +1,41 @@
 import subprocess
 
-# run mmseqs easy-search
-def run_mmseqs_easysearch(
-        query,
-        target,
+# create mmseqs database
+def mmseqs_createdb(fasta, outDB, threads):
+
+    # create compressed amino acid database from fasta
+    cmd = f'mmseqs createdb {str(fasta)} {str(outDB)} --db-type 2 --compressed -v 1 --threads {str(threads)}'
+
+    subprocess.run(cmd, shell=True, check=True)
+    return
+
+# concatenate two mmseqs databases
+def mmseqs_concatdbs(db1, db2, outDB, threads):
+
+    cmd = f'mmseqs concatdbs {str(db1)} {str(db2)} {str(outDB)} --preserve-keys --compressed -v 1 --threads {str(threads)}'
+    
+    subprocess.run(cmd, shell=True, check=True)
+    return
+
+# run mmseqs search
+def run_mmseqs_search(
+        querydb,
+        targetdb,
         outdir,
         tmpdir,
+        fident,
+        coverage,
         threads):
 
-    cmd = "mmseqs easy-search "
+    # basic inputs/outputs
+    cmd = f'mmseqs search {str(querydb)} {str(targetdb)} {str(outdir)} {str(tmpdir)} '
     
-    cmd += str(query) + " "
+    # translated AA search, align
+    cmd += f' --search-type 2 --alignment-mode 1 --cov-mod 0 -c {str(coverage)} '
     
-    cmd += str(target) + " "
+    cmd += f'--min-seq-id {str(fident)} --start-sens 1 --sens-steps 3 -s 7 '
     
-    cmd += str(outdir) + " " 
-
-    cmd += str(tmpdir)
-    
-    cmd += ' --search-type 2 --format-mode 4 -a --format-output "query,target,fident,alnlen,qlen,tlen,evalue" -v 1 --threads '
-    
-    cmd += str(threads)
+    cmd += f' --format-mode 4 --format-output "query,target,fident,alnlen,qlen,tlen,evalue" -v 1 --threads {str(threads)}'
 
     subprocess.run(cmd, shell=True, check=True)
 
