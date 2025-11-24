85380a2109ccf2f24859a01d67790c49823e1a59
Jacqueline Toussaint <lilyjacqueline@ebi.ac.uk>
2025-11-24 18:44:12 +0000

    add stdout and stderr warnings

--- pangenomerge/run_mmseqs.py
+++ pangenomerge/run_mmseqs.py
@@ -9,16 +9,20 @@ def mmseqs_createdb(fasta, outdb, threads, nt2aa: bool):
         # create nt database:
         tempfile = f'{str(outdb)}_nt'
         cmd = f'mmseqs createdb {str(fasta)} {str(tempfile)} --compressed 1 -v 3 --threads {str(threads)}'
-        subprocess.run(cmd, shell=True, check=True, capture_output=True)
+        subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+        check_result(result)
 
         # convert from nt to amino acid db:
         cmd = f'mmseqs translatenucs {str(tempfile)} {str(outdb)} --compressed 1 -v 3 --threads {str(threads)}'
-        subprocess.run(cmd, shell=True, check=True, capture_output=True)
+        subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+        check_result(result)
 
     if nt2aa is False:
         # create amino acid db:
         cmd = f'mmseqs createdb {str(fasta)} {str(outdb)} --compressed 1 -v 3 --threads {str(threads)}'
-        subprocess.run(cmd, shell=True, check=True, capture_output=True)
+        subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+        check_result(result)
+
     
     return
 
@@ -27,12 +31,15 @@ def mmseqs_concatdbs(db1, db2, outdb, tmpdir, threads):
 
     cmd = f'mmseqs concatdbs {str(db1)} {str(db2)} {str(outdb)} --compressed 1 -v 3 --threads 1'
     
-    subprocess.run(cmd, shell=True, check=True, capture_output=True)
-   
+    subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+    check_result(result)
+
     # now create the header database for outdb (doesn't happen automatically)
     
     cmd = f"mmseqs concatdbs {str(db1)} {str(db2)} {str(outdb)} --threads 1"
-    subprocess.run(cmd, shell=True, check=True, capture_output=True)
+
+    subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+    check_result(result)
 
     return
 
@@ -48,7 +55,8 @@ def run_mmseqs_search(
         threads):
 
     # remove any existing results db
-    subprocess.run(f'rm -f -- {str(resultdb)}*', shell=True, check=True, capture_output=True)
+    result = subprocess.run(f'rm -f -- {str(resultdb)}*', shell=True, check=True, capture_output=True, text=True)
+    check_result(result)
 
     # basic inputs/outputs
     cmd = f'mmseqs search {str(querydb)} {str(targetdb)} {str(resultdb)} {str(tmpdir)} '
@@ -62,11 +70,20 @@ def run_mmseqs_search(
     # default mmseqs sensitivity is 5.7 so can lower last step to speed up if needed
     cmd += f' --min-seq-id {str(fident)} --start-sens 1 --sens-steps 3 -s 5.7 -v 3 --threads {str(threads)}'
     
-    subprocess.run(cmd, shell=True, check=True, capture_output=True)
+    subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+    check_result(result)
 
     # output format, verbosity, and threads
     cmd = f' mmseqs convertalis {str(querydb)} {str(targetdb)} {str(resultdb)} {str(resultm8)} --format-mode 4 --format-output "query,target,fident,alnlen,qlen,tlen,evalue" -v 3 --threads {str(threads)}'
 
-    subprocess.run(cmd, shell=True, check=True, capture_output=True)
+    subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+    check_result(result)
 
     return
+
+def check_result(result):
+    if result.returncode != 0:
+        logging.error(f"MMseqs command failed: {cmd}")
+        logging.error(f"STDOUT:\n{result.stdout}")
+        logging.error(f"STDERR:\n{result.stderr}")
+        result.check_returncode()
