8c7260f7165cf22a823fd65124baf8cf98557c2c
Jacqueline Toussaint <lilyjacqueline@ebi.ac.uk>
2026-08-25 18:30:00 +0100

    add postprocess and MSA rules to the snakemake pipeline

---
+++ pangenomerge-postprocess-runner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""Convenience wrapper for running pangenomerge-postprocess directly from source tree."""
+
+from pangenomerge.generate_output import cli_main
+
+if __name__ == '__main__':
+    cli_main()
